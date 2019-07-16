8148a3efbe020d19e488da46a3266463974e3e49
Jennifer169 <52780850+Jennifer169@users.noreply.github.com>
2019-07-16 14:36:40 +0800

    Update HelloServlet.java

--- src/main/java/HelloServlet.java
+++ src/main/java/HelloServlet.java
@@ -10,7 +10,7 @@ public class HelloServlet extends HttpServlet{
     public void doGet(HttpServletRequest request, HttpServletResponse response){
 
         try {
-            response.getWriter().println("<h1>Hi Servlet!</h1>");
+            response.getWriter().println("<h1>Hi Servlet! this is v2.</h1>");
             response.getWriter().println(new Date());
         } catch (IOException e) {
             // TODO Auto-generated catch block
